9c986a6edffbc2a9b832dc20d1993145cdc9891e
Madondo07 <dumisanemadondo926@gmail.com>
2025-10-02 00:15:05 +0200

    Enrollment System

---
+++ OneDrive/Documents/NetBeansProjects/enrollment_system/src/main/java/za/ac/mycput/enrollment_system/Enrollment_system.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package za.ac.mycput.enrollment_system;
+
+/**
+ *
+ * @author DELL
+ */
+public class Enrollment_system {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
